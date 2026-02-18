8ac6b8f1b708e877319ef4d034d8a81552b6ad3a
yu-ri <yuri2517300028@gmail.com>
2026-02-18 21:50:04 +0900

    feat: added ft_env

---
+++ src/built_in/env/ft_env.c
@@ -0,0 +1,19 @@
+#include "minishell.h"
+
+int	ft_env(t_list *env_list)
+{
+	t_list	*current;
+	t_env	*env;
+
+	current = env_list;
+	while (current)
+	{
+		env = (t_env *)current->content;
+		ft_putstr_fd(env->key, STDOUT_FILENO);
+		ft_putstr_fd("=", STDOUT_FILENO);
+		ft_putstr_fd(env->value, STDOUT_FILENO);
+		write(STDOUT_FILENO, "\n", 1);
+		current = current->next;
+	}
+	return (SUCCESS);
+}
