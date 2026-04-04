5c651585b796970afcc591f8a9802048380236f5
K4JIC <Tomoya0815zak@gmail.com>
2026-04-04 18:20:14 +0900

    feat: merge frontend-parser branch

--- src/frontend/frontend.c
+++ src/frontend/frontend.c
@@ -14,11 +14,12 @@ int	frontend(char *input, t_minishell *ms)
 	print_all_token(tk_head);
 	printf("--end--");
 	ms->tokens = tk_head;
-	// ret = convert_token_to_cmd(ms, &cmd_head, tk_head);
-	// if (ret == FAILURE)
-		// return (FAILURE);
-	// printf("--cmd--");
-	// print_all_cmd(cmd_head);
-	// printf("--end--");
-	// ms->cmd_list = cmd_head;
+	ret = convert_token_to_cmd(ms, &cmd_head, tk_head);
+	if (ret == FAILURE)
+		return (FAILURE);
+	printf("--cmd--");
+	print_all_cmd(cmd_head);
+	printf("--end--");
+	ms->cmd_list = cmd_head;
+	return (SUCCESS);
 }
