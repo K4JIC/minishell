ebb04f537965af5ae81ea6ecbb9ca4459e6bd846
K4JIC <Tomoya0815zak@gmail.com>
2026-04-04 18:13:38 +0900

    chore: add frontend orchestrator.

---
+++ src/frontend/frontend.c
@@ -0,0 +1,24 @@
+#include "minishell.h"
+
+int	frontend(char *input, t_minishell *ms)
+{
+	t_token	*tk_head;
+	t_cmd	*cmd_head;
+	int		ret;
+
+	tk_head = convert_line_to_token(input);
+	if (tk_head == NULL)
+		return (FAILURE);
+	set_all_op_lavel(tk_head);
+	printf("--token--");
+	print_all_token(tk_head);
+	printf("--end--");
+	ms->tokens = tk_head;
+	// ret = convert_token_to_cmd(ms, &cmd_head, tk_head);
+	// if (ret == FAILURE)
+		// return (FAILURE);
+	// printf("--cmd--");
+	// print_all_cmd(cmd_head);
+	// printf("--end--");
+	// ms->cmd_list = cmd_head;
+}
